1e0a83e2e1bb387d2598bc77be471a2ce49fd437
jayittipon <ittiponjackeaw@gmail.com>
2021-06-13 15:58:56 +0700

    แก้ไข Join ตางรางระหว่าง Activity กับ Category

--- app/Category.php
+++ app/Category.php
@@ -6,13 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    public function posts()
-    {
+    public function activities(){
         return $this->hasMany(Activity::class);
-    }
-
-    public function category()
-    {
-        return $this->belongsTo(Category::class);
-    }
+      }
 }
